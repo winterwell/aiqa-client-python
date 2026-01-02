8231406d6c64c798933c9266eb77778b7eecd535
Daniel Winterstein <daniel.winterstein@n-able.com>
2026-01-02 13:27:54 +0000

    tweak

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 # Assumes setup has been done
 echo "Publishing to PyPI"
-echo "Assumes setup has been done. That is: python -m venv .venv, pip install build twine, .pypirc file created"
+echo "Assumes setup has been done. That is: python -m venv .venv, source .venv/bin/activate, pip install build twine, .pypirc file created"
 
 echo "Removing old builds: rm -rf dist/ build/ *.egg-info"
 rm -rf dist/ build/ *.egg-info
